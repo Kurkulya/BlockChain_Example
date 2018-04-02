cae0f822c4aab7f8cf888325012b6bf85dc88930
Kurkulya <kulinich_v@hotmail.com>
2018-04-02 12:09:11 +0300

    git added mining blocks

--- blockchain.js
+++ blockchain.js
@@ -1,17 +1,61 @@
 var sha256 = require('js-sha256').sha256;
 
+var difficulty = 6;
+var blocks = [];
+
 function Block (data, prevHash) {
+    this.nonce = 0;
     this.data = data;
     this.prevHash = prevHash;
     this.timeStamp = Date.now();
-    this.hash = sha256(prevHash + this.timeStamp.toString() + data);
+    this.calculateHash = function () {
+        return sha256(prevHash + this.timeStamp.toString() + this.nonce.toString() + data);
+    };
+    this.hash = this.calculateHash();
+    this.mineBlock = function (diff) {
+        let target = Array(diff + 1).join("0");
+        while(!(this.hash.slice(0, diff) === target)) {
+			this.nonce++;
+			this.hash = this.calculateHash();
+		}
+        console.log("Block mined: " + this.hash);
+    }
 }
 
-var firstBlock = new Block("Hi, i'm first block", "0");
-console.log("Hash for block 1: " + firstBlock.hash);
+function isChainValid () {
+    var curBlock;
+    var prevBlock;
+    var hashTarget = Array(difficulty + 1).join("0");
+    for (let i = 1; i < blocks.length; i++) {
+        curBlock = blocks[i];
+        prevBlock = blocks[i-1];
+        if (!(curBlock.hash === curBlock.calculateHash())) {
+            console.log("Current hashes are not equal!"); 
+            return false;
+        }
+        if (!(prevBlock.hash === prevBlock.calculateHash())) {
+            console.log("Current hashes are not equal!");
+            return false;
+        }
+        if (!(curBlock.hash.substr(0, difficulty) === hashTarget)) {
+            console.log("This block hasn't been mined!");
+            return false;
+        }
+    }
+    console.log("Chain is valid!");
+    return true;
+}
 
-var secondBlock = new Block("Hi, i'm second block", firstBlock.hash);
-console.log("Hash for block 2: " + firstBlock.hash);
+blocks.push(new Block("Hi, i'm first block", "0"));
+console.log("Trying to mine block 1...");
+blocks[0].mineBlock(difficulty);
+blocks.push(new Block("Hi, i'm second block", blocks[blocks.length - 1].hash));
+console.log("Trying to mine block 2...");
+blocks[1].mineBlock(difficulty);
+blocks.push(new Block("Hi, i'm third block", blocks[blocks.length - 1].hash));
+console.log("Trying to mine block 3...");
+blocks[2].mineBlock(difficulty);
 
-var thirdBlock = new Block("Hi, i'm third block", secondBlock.hash);
-console.log("Hash for block 3: " + firstBlock.hash);
+isChainValid();
+
+console.log(blocks);
